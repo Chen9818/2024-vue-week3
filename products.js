e1f71e5fbb9f036f523ad01db8fedac8c2aa16b0
WEI <wwenba2k@gmail.com>
2024-01-21 22:19:23 +0900

    vue-week4

--- products.js
+++ products.js
@@ -1,18 +1,17 @@
-// 產品資料格式
 import { createApp, ref, onMounted } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 
 let productModal = null;
 let delProductModal = null;
 
-createApp({
-  setup(){
+const app = createApp({
+  setup() {
     const apiUrl = 'https://vue3-course-api.hexschool.io/v2';
     const apiPath = 'wei-c';
+    
     const products = ref([]);
-    const tempProduct = ref({
-      imagesUrl: [],
-    });
-    const status = ref(false);
+    const tempProduct = ref({ imagesUrl: [] });
+    const pagination = ref({});
+    const isNew = ref(false);
 
     //首先確認是否登入
     const checkAdmin = () => {
@@ -22,37 +21,95 @@ createApp({
           getData();
         })
         .catch((err) => {
+          alert(err.response.data.message)
+          window.location = 'login.html';
+        });
+    };
+
+    const getData = (page = 1) => {
+      const url = `${apiUrl}/api/${apiPath}/admin/products?page=${page}`;
+
+      axios.get(url)
+        .then((response) => {
+          products.value = response.data.products;
+          pagination.value = response.data.pagination;
+        }).catch((err) => {
           alert(err.response.data.message);
           window.location = 'login.html';
         });
     };
 
-    const getData = () => {
-      const url = `${apiUrl}/api/${apiPath}/admin/products`;
-      axios.get(url)
-        .then((response) => {
-          products.value = response.data.products;
-        })
-        .catch((err) => {
-          alert(err.response.data.message);
-        });
+    const openModal = (type, item) => {
+      if (type === 'new') {
+        tempProduct.value = { imagesUrl: [] };
+        isNew.value = true;
+        productModal.show();
+      } else if (type === 'edit') {
+        tempProduct.value = { ...item };
+        isNew.value = false;
+        productModal.show();
+      } else if (type === 'delete') {
+        tempProduct.value = { ...item };
+        delProductModal.show();
+      }
     };
 
+    onMounted(() => {
+      const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+      //取得token
+      axios.defaults.headers.common.Authorization = token;
+      //預設夾帶token至header
+      checkAdmin();
+    });
+
+    return {
+      products,
+      tempProduct,
+      pagination,
+      isNew,
+      getData,
+      openModal,
+    };
+  },
+});
+
+// 分頁元件
+app.component('pagination', {
+  template: '#pagination',
+  props: ['pages'],
+  setup(props, { emit }) {
+    const emitPages = (item) => {
+      emit('emit-pages', item);
+    };
+
+    return {
+      emitPages,
+    };
+  }
+});
+
+//新增編輯商品元件
+app.component('productModal', {
+  template: '#productModal',
+  props: ['isNew', 'product'],
+  setup(props, { emit }) {
+    const apiUrl = 'https://vue3-course-api.hexschool.io/v2';
+    const apiPath = 'wei-c';
 
     const updateProduct = () => {
-        let url = `${apiUrl}/api/${apiPath}/admin/product`;
-        let http = 'post';//新增
-  
-        if (!status.value) {
-          url = `${apiUrl}/api/${apiPath}/admin/product/${tempProduct.value.id}`;
-          http = 'put';//編輯
-        }
-  
-        axios[http](url, { data: tempProduct.value })
-          .then((response) => {
-            alert(response.data.message);
-            productModal.hide();
-            getData();
+      let api = `${apiUrl}/api/${apiPath}/admin/product`;
+      let httpMethod = 'post';//新增
+
+      if (!props.isNew) {
+        api = `${apiUrl}/api/${apiPath}/admin/product/${props.product.id}`;
+        httpMethod = 'put';//編輯
+      }
+
+      axios[httpMethod](api, { data: props.product })
+        .then((response) => {
+          alert(response.data.message);
+          hideModal();
+          emit('update');
         })
         .catch((err) => {
           alert(err.response.data.message);
@@ -60,72 +117,68 @@ createApp({
     };
 
     const createImages = () => {
-      tempProduct.value.imagesUrl = [];
-      tempProduct.value.imagesUrl.push('');
+      props.product.imagesUrl = [];
+      props.product.imagesUrl.push('');
     };
 
+    const hideModal = () => {
+      productModal.hide();
+    };
 
-
-    const openModal = (isNew, item) => {
-        console.log('test')
-        if (isNew === 'new') {
-          tempProduct.value = {
-            imagesUrl: [],
-          };
-          status.value = true;
-          productModal.show();
-        } else if (isNew === 'edit') {
-          tempProduct.value = { ...item };
-          status.value = false;
-          productModal.show();
-        } else if (isNew === 'delete') {
-          tempProduct.value = { ...item };
-          delProductModal.show();
-        }
-      };
-  
-      const delProduct = () => {
-        const url = `${apiUrl}/api/${apiPath}/admin/product/${tempProduct.value.id}`;
-  
-        axios.delete(url)
-          .then((response) => {
-            alert(response.data.message);
-            delProductModal.hide();
-            getData();
-          })
-          .catch((err) => {
-            alert(err.response.data.message);
-          });
-      };
-  
-  
-    
     onMounted(() => {
       //標記BS5 modal
       productModal = new bootstrap.Modal(document.getElementById('productModal'), {
         keyboard: false,
+        backdrop: 'static',
       });
-
-      delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'), {
-        keyboard: false,
-      });
-
-      const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
-      //取得token
-      axios.defaults.headers.common['Authorization'] = token;
-      //預設夾帶token至header
-
-      checkAdmin();
     });
 
-    return{
-      products,
-      tempProduct,
-      status,
+    return {
       updateProduct,
       createImages,
-      openModal,
+    };
+  },
+});
+
+// 產品刪除元件
+app.component('delProductModal', {
+  template: '#delProductModal',
+  props: ['item'],
+  setup(props, { emit }) {
+    const apiUrl = 'https://vue3-course-api.hexschool.io/v2';
+    const apiPath = 'wei-c';
+
+    const delProduct = () => {
+      axios.delete(`${apiUrl}/api/${apiPath}/admin/product/${props.item.id}`)
+        .then(() => {
+          emit('update');
+          hideModal();
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    };
+
+    const openModal = () => {
+      delProductModal.show();
+    };
+
+    const hideModal = () => {
+      delProductModal.hide();
+    };
+
+    onMounted(() => {
+      delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'), {
+        keyboard: false,
+        backdrop: 'static',
+      });
+    });
+
+    return {
       delProduct,
-    }
-  }
-}).mount('#app');
+      openModal,
+    };
+  },
+});
+
+app.mount('#app');
